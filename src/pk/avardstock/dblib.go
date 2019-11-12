9403e553afc97f740748b92774ca4b0e72f9cc0f
taaheri <taaheri@outlook.com>
2019-11-12 16:03:42 +0330

    ff

---
+++ src/pk/avardstock/dblib.go
@@ -0,0 +1,35 @@
+package avardstock
+import (
+
+	"fmt"
+
+   "github.com/jinzhu/gorm"
+   _ "github.com/jinzhu/gorm/dialects/sqlite"
+   "database/sql"
+)
+
+
+type Nemad struct {
+   ID	string
+   GroupCode   string
+	GroupName	string
+   NemadCode string
+   NameEn string
+  NameFa string
+  NameFull string
+}
+
+func DatabaseInit() error {
+   
+   db, err := gorm.Open("sqlite3", "./stock.db")
+  if err != nil {
+    panic("failed to connect database")
+  }
+  defer db.Close()
+
+  // Migrate the schema
+  db.AutoMigrate(&Nemad{})
+
+  
+    return nil
+}
